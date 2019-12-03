636ce58dfe4f564c6bff771afba1c740f784cff0
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-12-03 14:25:58 +0100

    Quicksort algorithm solved

--- Python-Sorting-Algorithms/quicksort.py
+++ Python-Sorting-Algorithms/quicksort.py
@@ -1,3 +1,12 @@
+"""
+Quick sort
+
+Quicksort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined.
+
+Best Case: O(n*log(n)) 
+Average: O(n*log(n))
+Worst Case: O(n^2)
+"""
 
 
 def swap(l: list, index1, index2):
@@ -5,31 +14,29 @@ def swap(l: list, index1, index2):
     l[index1] = l[index2]
     l[index2] = temp
 
-def quicksort(unsorted: list, start, end) -> list:
-    
-    if start == end:
-        return [unsorted[start]]
 
-    pivot = unsorted[end]
-    j = 0
+def partition(arr: list, start, end):
+    pivot = arr[end]
+    j = start
     for i in range(start, end):
-        if unsorted[i] < pivot:
-            swap(unsorted, i, j)
+        if arr[i] < pivot:
+            swap(arr, i, j)
             j += 1
-    swap(unsorted, j, end)
-    #print(start, j)
+    swap(arr, j, end)
+    return j
 
-    #print("quicksort({}, {}, {}) j = {}".format(unsorted[start:end + 1], start, end, j))
-    if j == 0:
-        left = quicksort(unsorted, start, j)
-    else:
-        left = quicksort(unsorted, start, j - 1)
 
-    if j == len(unsorted):
-        right = quicksort(unsorted, j, end)
-    else:
-        right = quicksort(unsorted, j + 1, end)
-    return left + [pivot] + right
+def quicksort(unsorted: list, start, end) -> list:
 
-    
-print(quicksort([10, 7, 12, 8, 3, 2, 6], 0, 6))
+    if start >= end:
+        return
+
+    p = partition(unsorted, start, end)
+    quicksort(unsorted, start, p - 1)
+    quicksort(unsorted, p + 1, end)
+
+    return unsorted
+
+
+if __name__ == "__main__":
+    print(quicksort([10, 7, 12, 8, 3, 2, 6], 0, 6))
