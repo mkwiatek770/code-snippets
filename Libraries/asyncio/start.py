c11134e6b0195c34117a99ac546f86f69a7e79c2
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-04 19:47:54 +0200

    Its hard to measure time of decorated coroutine function

--- Libraries/asyncio/start.py
+++ Libraries/asyncio/start.py
@@ -1,5 +1,6 @@
 import math
 import time
+import asyncio
 
 def timer(func):
 
@@ -7,12 +8,12 @@ def timer(func):
         t0 = time.perf_counter()
         value = func(*args, **kwargs)
         t1 = time.perf_counter()
-        print(f"Time took: {t1-t0:.2f}")
+        print(f"Time took: {t1-t0:.2f} seconds")
         return value
     return inner
 
-@timer
-def find_primes_in_range(first, last):
+
+async def find_primes_in_range(first, last):
     primes = []
     for num in range(first, last + 1):
         is_prime = True
@@ -25,7 +26,21 @@ def find_primes_in_range(first, last):
     print(f"Found {len(primes)} primes in range from {first} to {last}")
     return primes
 
+
+async def main():
+    primes1 = loop.create_task(find_primes_in_range(200_000, 1_000_000))
+    primes2 = loop.create_task(find_primes_in_range(50_000, 100_000))
+    primes3 = loop.create_task(find_primes_in_range(50, 1000))
+    await asyncio.wait([primes1, primes2, primes3])
+
 if __name__ == "__main__":
-    find_primes_in_range(200_000, 1_000_000)
-    find_primes_in_range(50_000, 100_000)
-    find_primes_in_range(50, 1000)
+    t0 = time.perf_counter()
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    except Exception:
+        pass
+    finally:
+        # is it necessary?
+        loop.close()
+        print(f"Time took: {time.perf_counter() - t0:.2f} seconds")
