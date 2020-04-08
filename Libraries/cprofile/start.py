cb2f4373ea4457a740e3b17a8603289f4657cacc
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-08 12:16:44 +0200

    Decorator that would enable to profile code

---
+++ Libraries/cprofile/start.py
@@ -0,0 +1,20 @@
+import cProfile
+import pstats
+import io
+
+def profile(func):
+    """Profile function decorator."""
+    
+    def inner(*args, **kwargs):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        response = func(*args, **kwargs)
+        profiler.disable()
+
+        stream = io.StringIO()
+        ps = pstats.Stats(profiler, stream=stream).sort_stats('cumulative')
+        ps.print_stats()
+        print(s.getvalue())
+        return response
+    return inner
+
