1d5602aac125f0640fdaf3b9b74698f0380b9360
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-08 20:15:27 +0200

    Make bubble sort alorithm faster

--- Python-Sorting-Algorithms/bubble.py
+++ Python-Sorting-Algorithms/bubble.py
@@ -10,18 +10,29 @@ Worst Case: O(n^2)
 
 
 def bubble(unsorted: list) -> list:
-    while True:
-        swaps = 0
+    swaps = True
+    while swaps:
+        swaps = False
         for i in range(len(unsorted) - 1):
             if unsorted[i] > unsorted[i + 1]:
                 unsorted[i], unsorted[i + 1] = unsorted[i + 1], unsorted[i]
-                swaps += 1
-        if swaps == 0:
-            break
+                swaps = True
     return unsorted
 
 
+# Better way
+def bubble(collection):
+    for crossing in range(len(collection) - 1):
+        for i in range(len(collection) - crossing -1):
+            first = collection[i]
+            second = collection[i + 1]
+            if first > second:
+                collection[i], collection[i+1] = collection[i+1], collection[i]
+    return collection
+
+
+
 if __name__ == "__main__":
-    start = [5, 1, 4, 2, 8]
+    start = [5, 1, 4, 8, 2]
     print("Start array: ", start)
     print("Result: ", bubble(start))
