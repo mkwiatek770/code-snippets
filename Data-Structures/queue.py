e8b8a21133b30f25cc610ffeb5c67acec134216c
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-09 14:31:44 +0200

    Queue implementation

---
+++ Data-Structures/queue.py
@@ -0,0 +1,54 @@
+"""
+FIFO - First In First Out
+"""
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def is_empty(self) -> bool:
+        return self.head is None
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.head.data
+
+    def add(self, data):
+        node = Node(data)
+        
+        if self.tail is not None:
+            self.tail.next = node
+        self.tail = node
+        if self.head is None:
+            self.head = node
+
+    def remove(self):
+        if self.is_empty():
+            return False
+
+        value = self.head.data
+        self.head = self.head.next
+        if self.head is None:
+            self.tail = None
+        return value
+
+
+q = Queue()
+q.add(14)
+q.add(6)
+assert q.head.data == 14
+assert q.tail.data == 6
+q.remove()
+assert q.head.data == 6
+assert q.tail.data == 6
+
