d56145939f2d1e919c614e3fc85c6d827596e1bb
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-10 17:59:20 +0200

    Get rid of old MinIntHeap code

--- Data-Structures/heap.py
+++ Data-Structures/heap.py
@@ -97,80 +97,7 @@ class BinaryHeap(ABC):
 
 class MinIntHeap:
 
-    def __init__(self):
-        self.capacity: int = 10
-        self.items: list = []
-
-    def _get_left_child_index(self, parent_index: int) -> int:
-        return 2 * parent_index + 1
-
-    def _get_right_child_index(self, parent_index: int) -> int:
-        return 2 * parent_index + 2
-
-    def _get_parent_index(self, child_index: int) -> int:
-        return (child_index - 2) // 2
-
-    def has_left_child(self, index: int) -> bool:
-        return self._get_left_child_index(index) < len(self.items)
-
-    def has_right_child(self, index: int) -> bool:
-        return self._get_right_child_index(index) < len(self.items)
-
-    def has_parent(self, index: int) -> bool:
-        return self._get_parent_index(index) >= 0
-
-    def left_child(self, index: int) -> int:
-        return self.items[self._get_left_child_index(index)]
-
-    def right_child(self, index: int) -> int:
-        return self.items[self._get_right_child_index(index)]
-
-    def parent(self, index: int) -> int:
-        return self.items[self._get_parent_index(index)]
-
-    def swap(self, i, j):
-        self.items[i], self.items[j] = self.items[j], self.items[i]
-
-    def ensure_extra_capacity(self):
-        if len(self.items) == self.capacity:
-            self.items = self.items[:] * 2
-            self.capacity *= 2
-
-    def peek(self):
-        try:
-            return self.items[0]
-        except IndexError:
-            print("There has to be at least one element")
-
-    def poll(self):
-        item = self.items[0]
-        self.items[0] = self.items[len(self.items) - 1]
-        self.heapify_down()
-        return item
-
-    def add(self, item):
-        # self.ensure_extra_capacity()
-        self.items.append(item)
-        self.heapify_up()
-
-    def heapify_up(self):
-        index = len(self.items) - 1
-        while self.has_parent(index) and self.parent(index) > self.items[index]:
-            self.swap(self._get_parent_index(index), index)
-            index = self._get_parent_index(index)
-
-    def heapify_down(self):
-        index = 0
-        while (self.has_left_child(index)):
-            smaller_child_index = self._get_left_child_index(index)
-            if (self.has_right_child(index) and self.right_child(index) < self.left_child(index)):
-                smaller_child_index = self._get_right_child_index(index)
-
-            if self.items[index] > self.items[smaller_child_index]:
-                self.swap(index, smaller_child_index)
-            else:
-                break
-            index = smaller_child_index
+    pass
 
 
 class MaxIntHeap(BinaryHeap):
@@ -214,6 +141,21 @@ class MaxIntHeap(BinaryHeap):
                 break
             self.swap(index, bigger_child_index)
             index = bigger_child_index
+
+
+def test_heap_min():
+    heap = MinIntHeap()
+    heap.insert(15)
+    assert heap.min == 15
+    heap.insert(17)
+    assert heap.min == 15
+    heap.insert(4)
+    assert heap.min == 4
+    assert heap.items == [4, 17, 15]
+    heap.insert(16)
+    assert heap.items == [4, 16, 15, 17]
+    assert heap.delete() == 4
+    assert heap.items == [15, 16, 17]
 
 
 def test_heap_max():
