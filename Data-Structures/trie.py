f53965dbb54ad5ffa678530eb5fb67ee2ad702c3
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-12 21:02:55 +0200

    Trie class

--- Data-Structures/trie.py
+++ Data-Structures/trie.py
@@ -4,12 +4,26 @@ Sample use cases:
 - word syntax checher/tips, where word is getting underlined if its not found in db
 """
 
-class Node:
+class Trie:
     
-    def __init__(self, children=None, is_complete_word=None):
-        if children:
-            self.children = list(children)
-        else:
-            self.children = []
-        self.is_complete_word = is_complete_word
+    def __init__(self):
+        head = {}
 
+    def add(self, word):
+        cur = self.head
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur["*"] = True
+
+    def search(self, word):
+        cur = self.head
+
+        for c in word:
+            if not c in cur:
+                return False
+            cur = cur[c]
+        if "*" in cur:
+            return True
+        return False
