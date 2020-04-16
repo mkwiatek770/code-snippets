96168b8c5114b5257fcdb29781c88060394fc7af
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-16 16:22:31 +0200

    Load all json files into memory

--- Libraries/pandas/covid-19/corona.py
+++ Libraries/pandas/covid-19/corona.py
@@ -1,7 +1,16 @@
 import os
+import json
+import sys
 
 dirs = ["biorxiv_medrxiv", "comm_use_subset", "noncomm_use_subset"]
+articles = []
 
-for file_ in os.listdir(f"kaggle_data/{dirs[0]}/{dirs[0]}/pdf_json"):
+path = f"kaggle_data/{dirs[0]}/{dirs[0]}/pdf_json"
+for file_ in os.listdir(path):
     print(file_)
+    with open(f"{path}/{file_}") as f:
+        articles.append(json.load(f))
 
+
+print(articles[5])
+print(len(articles))
