8e34a0a16c3cdc8f45f19434f89a457aefadfebb
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-16 16:51:06 +0200

    Make use of tqdm library to see progress of time consuming taskss

--- Libraries/pandas/covid-19/corona.py
+++ Libraries/pandas/covid-19/corona.py
@@ -1,16 +1,17 @@
 import os
 import json
 import sys
+from tqdm import tqdm
 
 dirs = ["biorxiv_medrxiv", "comm_use_subset", "noncomm_use_subset"]
 articles = []
 
 path = f"kaggle_data/{dirs[0]}/{dirs[0]}/pdf_json"
-for file_ in os.listdir(path):
-    print(file_)
+for file_ in tqdm(os.listdir(path)):
     with open(f"{path}/{file_}") as f:
         articles.append(json.load(f))
 
 
+print(sys.getsizeof(articles))
 print(articles[5])
 print(len(articles))
