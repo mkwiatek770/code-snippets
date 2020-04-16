0cbe8f5e5f60cb302102b1ff69263a6ce866a0ea
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-16 17:01:31 +0200

    Load all articles and insert usefull informations into list to build dataframe from it

--- Libraries/pandas/covid-19/corona.py
+++ Libraries/pandas/covid-19/corona.py
@@ -1,6 +1,7 @@
 import os
 import json
 import sys
+import pandas as pd
 from tqdm import tqdm
 
 dirs = ["biorxiv_medrxiv", "comm_use_subset", "noncomm_use_subset"]
@@ -9,7 +10,21 @@ articles = []
 path = f"kaggle_data/{dirs[0]}/{dirs[0]}/pdf_json"
 for file_ in tqdm(os.listdir(path)):
     with open(f"{path}/{file_}") as f:
-        articles.append(json.load(f))
+        data = json.load(f)
+
+        paper_id = data['paper_id']
+        title = data['metadata']['title']
+
+        full_abstract = ""
+        for part in data['abstract']:
+            full_abstract += part['text'] + "\n"
+
+        full_text = ""
+        for text_part in data["body_text"]:
+            full_text += text_part["text"] + "\n\n"
+        
+        articles.append((paper_id, title, full_abstract, full_text))
+        
 
 
 print(sys.getsizeof(articles))
