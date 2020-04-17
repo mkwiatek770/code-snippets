5254b6a32ecb89ed830b99797c4f9a326840221a
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-17 20:19:39 +0200

    Seems to work ;)

--- Algorithms/sorting/mergesort.py
+++ Algorithms/sorting/mergesort.py
@@ -8,16 +8,18 @@ Its efficient, but downside is than it requires extra space
 
 def merge(arr1: list, arr2: list) -> list:
     new_arr = []
-    for i in arr1:
-        for j in arr2:
-            if j < i:
-                new_arr.append(j)
-                arr2.remove(j)
-            else:
-                new_arr.append(i)
-                arr1.remove(i)
-    return new_arr
+    # Iterate while at least one array is not empty
+    while len(arr1) != 0 and len(arr2) != 0:
+        if arr1[0] <= arr2[0]:
+            new_arr.append(arr1.pop(0))
+        else:
+            new_arr.append(arr2.pop(0))
+    
+    # At this point at least one array is empty add leftovers
+    new_arr += arr1
+    new_arr += arr2
 
+    return new_arr
 
 
 def mergesort(arr: list):
@@ -27,12 +29,13 @@ def mergesort(arr: list):
     first_half = arr[:len(arr) // 2]
     second_half = arr[len(arr) // 2:]
     
-    res1 = mergesort(first_half)
-    res2 = mergesort(second_half)
-    return res1, res2
+    left = mergesort(first_half)
+    right = mergesort(second_half)
+    
+    return merge(left, right)
 
 
-print(mergesort([1, 2, 3, 4, 5]))
+print(mergesort([4, -5, 2, 123, 11]))
 print(mergesort([1, 2, 3, 4]))
 
 #merge([1, 2], [4, 5])
