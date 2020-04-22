561a0c9381ab94b07198132a9108804e37cc9b83
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-22 15:52:27 +0200

    Slight changes in quicksort file

--- Algorithms/sorting/quicksort.py
+++ Algorithms/sorting/quicksort.py
@@ -2,6 +2,7 @@
 Quick sort
 
 Quicksort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined.
+- Divide and conquer method
 
 Best Case: O(n*log(n)) 
 Average: O(n*log(n))
@@ -18,7 +19,7 @@ def partition(arr: list, start, end):
     for i in range(start, end):
         if arr[i] < pivot:
             # swap(arr, i, j)
-            # python is so fucking awesome :)
+            # python is that fucking awesome :)
             arr[i], arr[j] = arr[j], arr[i]
             j += 1
     swap(arr, j, end)
