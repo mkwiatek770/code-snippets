bbcaeddadff205b5e2acdcaac05ab1f2e7e0f982
mkwiatek770 <michalkwiatek9@o2.pl>
2020-04-26 20:47:26 +0200

    Add comment

--- Classic-CS-Problems/1-Small-Problems/encryption.py
+++ Classic-CS-Problems/1-Small-Problems/encryption.py
@@ -8,6 +8,7 @@ def random_key(length: int) -> int:
     return int.from_bytes(tb, "big")
 
 
+# XOR based encrytpion
 def encrypt(original: str) -> Tuple[int, int]:
     original_bytes: bytes = original.encode()
     dummy: int = random_key(len(original_bytes))
@@ -16,3 +17,7 @@ def encrypt(original: str) -> Tuple[int, int]:
     return dummy, encrypted
 
 
+def decrypt(key1: int, key2: int) -> str:
+    decrypted: int = key1 ^ key2  # XOR
+    temp: bytes = decrypted.to_bytes((decrypted.bit_length()+ 7) // 8, "big")
+    return temp.decode()
