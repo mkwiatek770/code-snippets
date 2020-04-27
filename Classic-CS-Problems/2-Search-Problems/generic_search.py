683801a7522479f7432e50eed77298234cb1dc70
mkwiatek770 <michalkwiatek9@o2.pl>
2020-04-27 15:51:38 +0200

    Priority queue

--- Classic-CS-Problems/2-Search-Problems/generic_search.py
+++ Classic-CS-Problems/2-Search-Problems/generic_search.py
@@ -84,6 +84,25 @@ class Queue(Generic[T]):
         return repr(self._container)
 
 
+class PriorityQueue(Generic[T]):
+
+    def __init__(self) -> None:
+        self._container: List[T] = []
+    
+    @property
+    def empty(self) -> bool:
+        return not self._container
+    
+    def push(self, item: T) -> None:
+        heappush(self._container, item) # in by priority
+
+    def pop(self) -> T:
+        return heappop(self._container) # out by priority
+    
+    def repr(self) -> str:
+        return repr(self._container)
+
+ 
 class Node(Generic[T]):
 
     def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0):
@@ -139,6 +158,28 @@ def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Li
     return None
 
 
+def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]], 
+           heuristic: Callable[[T], float]) -> Optional[Node[T]]:\
+    
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node(initial, None))
+
+    explored: Dict[T, float] = {initial: 0.0}
+
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        if goal_test(current_state):
+            return current_node
+        
+        for child in successors(current_state):
+            new_cost: float = current_node.cost + 1
+            if child not in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node, new_cost, heuristic(child)))
+    return None
+
+
 def node_to_path(node: Node[T]) -> List[T]:
     path: List[T] = [node.state]
     while node.parent:
@@ -146,6 +187,7 @@ def node_to_path(node: Node[T]) -> List[T]:
         path.append(node.state)
     path.reverse()
     return path
+
 
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5)) # True
