470a9f35c7a8255df5a6bb594a03d6af2d33d2ae
mkwiatek770 <michalkwiatek9@o2.pl>
2020-04-27 14:32:11 +0200

    Depth first search algorithm

--- Classic-CS-Problems/2-Search-Problems/generic_search.py
+++ Classic-CS-Problems/2-Search-Problems/generic_search.py
@@ -54,10 +54,10 @@ class Stack(Generic[T]):
     def empty(self) -> bool:
         return self._container == []
     
-    def pop(self, item: T) -> None:
+    def push(self, item: T) -> None:
         self._container.append(item)
     
-    def push(self) -> T:
+    def pop(self) -> T:
         if not self.empty:
             return self._container.pop()
 
@@ -76,6 +76,28 @@ class Node(Generic[T]):
     def __lt__(self, other: Node) -> bool:
         return (self.cost + self.heuristic) < (other.cost + other.heuristic)
 
+
+def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    # frontier is where we've yet to go
+    frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+    # explored is where we've been
+    explored: Set[T] = {initial}
+
+    # keep going while there is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # if we found the goal, we're done
+        if goal_test(current_state):
+            return current_node
+        # check where we can go next and haven't explored
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None # went through everything and never found goal
 
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5)) # True
