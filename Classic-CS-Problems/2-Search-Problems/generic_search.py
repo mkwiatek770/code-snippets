50fc6cb3d961eeb5c56ef29d3402c12b70797efe
mkwiatek770 <michalkwiatek9@o2.pl>
2020-04-27 15:26:19 +0200

    Implement breadth-first search algorithm

--- Classic-CS-Problems/2-Search-Problems/generic_search.py
+++ Classic-CS-Problems/2-Search-Problems/generic_search.py
@@ -65,6 +65,25 @@ class Stack(Generic[T]):
         return repr(self._container)
 
 
+class Queue(Generic[T]):
+
+    def __init__(self) -> None:
+        self._container: Deque[T] = Deque()
+    
+    @property
+    def empty(self) -> bool:
+        return not self._container
+    
+    def push(self, item: T) -> None:
+        self._container.append(item)
+    
+    def pop(self) -> T:
+        return self._container.popleft() # FIFO
+    
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
 class Node(Generic[T]):
 
     def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0):
@@ -98,6 +117,26 @@ def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Li
             explored.add(child)
             frontier.push(Node(child, current_node))
     return None # went through everything and never found goal
+
+
+def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    frontier: Queue[Node[T]] = Queue()
+    frontier.push(Node(initial, None))
+    
+    explored: Set[T] = {initial}
+
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+
+        if goal_test(current_state):
+            return current_node
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None
 
 
 def node_to_path(node: Node[T]) -> List[T]:
