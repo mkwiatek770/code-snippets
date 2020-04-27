73d0728fe5032b7a901a05f8c43d70b78b73b5d8
mkwiatek770 <michalkwiatek9@o2.pl>
2020-04-27 14:21:08 +0200

    Implement stack and node classes

--- Classic-CS-Problems/2-Search-Problems/generic_search.py
+++ Classic-CS-Problems/2-Search-Problems/generic_search.py
@@ -45,6 +45,38 @@ def binary_contains(sequence: Sequence[C], key: C) -> bool:
     return False
 
 
+class Stack(Generic[T]):
+
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return self._container == []
+    
+    def pop(self, item: T) -> None:
+        self._container.append(item)
+    
+    def push(self) -> T:
+        if not self.empty:
+            return self._container.pop()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+class Node(Generic[T]):
+
+    def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0):
+        self.state: T = state
+        self.parent: Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+    
+    def __lt__(self, other: Node) -> bool:
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
+
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5)) # True
     print(binary_contains(["a", "d", "e", "f", "z"], "f")) # True
