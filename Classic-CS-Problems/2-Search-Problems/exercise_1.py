e5f095ce6a144184e22d9de761cd8dcf015c93f7
mkwiatek770 <michalkwiatek9@o2.pl>
2020-04-28 18:23:32 +0200

    Make tests more verbose

--- Classic-CS-Problems/2-Search-Problems/exercise_1.py
+++ Classic-CS-Problems/2-Search-Problems/exercise_1.py
@@ -17,7 +17,7 @@ class TestSearchPerformance(unittest.TestCase):
         cls.long_list: List[int] = [i for i in range(1_000_000)]
     
     def test_linear_search_item_exist(self):
-
+        print("linear search")
         # test easy case
         t0 = time.perf_counter()
         res = linear_contains(self.long_list, 1000)
@@ -47,26 +47,26 @@ class TestSearchPerformance(unittest.TestCase):
         self.assertFalse(res)
     
     def test_binary_search(self):
-
+        print("binary search")
         # test easy case
         t0 = time.perf_counter()
         res = binary_contains(self.long_list, 500_000)
         t1 = time.perf_counter()
-        print(f"To find item 1000 it took {t1-t0} secs")
+        print(f"To binary find item 1000 it took {t1-t0} secs")
         self.assertTrue(res)
 
         # test hard case
         t0 = time.perf_counter()
         res = binary_contains(self.long_list, 500_001)
         t1 = time.perf_counter()
-        print(f"To find item 500_001 it took {t1-t0} secs")
+        print(f"To binary find item 500_001 it took {t1-t0} secs")
         self.assertTrue(res)
 
         # test non existing item
         t0 = time.perf_counter()
         res = binary_contains(self.long_list, -1)
         t1 = time.perf_counter()
-        print(f"To find item -1 it took {t1-t0} secs")
+        print(f"To binary find item -1 it took {t1-t0} secs")
         self.assertFalse(res)
 
 
