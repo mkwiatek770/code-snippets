4c0ab14284c2153b775e5d2fe0280a6c617cecc5
mkwiatek770 <michalkwiatek9@o2.pl>
2020-04-29 11:57:01 +0200

    Fix typos

--- Classic-CS-Problems/3-Constraint-Satusfaction-Problems/csp.py
+++ Classic-CS-Problems/3-Constraint-Satusfaction-Problems/csp.py
@@ -11,7 +11,7 @@ class Constraint(Generic[V, D], ABC):
         self.variables = variables
     
     @abstractmethod
-    def statisfied(self, assignment: Dict[V, D]) -> bool:
+    def satisfied(self, assignment: Dict[V, D]) -> bool:
         pass
 
 
@@ -32,4 +32,26 @@ class CSP(Generic[V, D]):
                 raise LookupError("Variable in constraint not in CSP")
             else:
                 self.constraints[variable].append(constraint)
+    
+    def consistent(self, variable: V, assignment: Dict[V, D]) -> bool:
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
+    
+    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+        if len(assignment) == len(self.variables):
+            return assignment
         
+        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+
+        first: V = unassigned[0]
+        for value in self.domains[first]:
+            local_assignment = assignment.copy()
+            local_assignment[first] = value
+            # if we're still consistent we recuse (continue)
+            if self.consistent(first, local_assignment):
+                result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
+                if result is not None:
+                    return result
+        return None
