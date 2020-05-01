9bf4b1e7fb47f8ebeff5804571c557c949ce40e8
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-01 18:50:13 +0200

    Add vertices represented by USA cities

--- Classic-CS-Problems/4-Graph-Problems/graph.py
+++ Classic-CS-Problems/4-Graph-Problems/graph.py
@@ -60,3 +60,38 @@ class Graph(Generic[V]):
         for i in range(self.vertex_count):
             desc += f"{self.vertex_at(i)} -> {self.neighbours_for_index(i)}\n"
         return desc
+
+
+if __name__ == "__main__":
+    # test basic Graph construction
+    city_graph: Graph[str] = Graph(["Seattle", "San Francisco", "Los Angeles", 
+    "Riverside", "Phoenix", "Chicago", "Boston", "New York",
+    "Atlanta", "Miami", "Dallas", "Houston", "Detroit", "Philadelphia",
+    "Washington"])
+    city_graph.add_edge_by_vertices("Seattle", "Chicago")
+    city_graph.add_edge_by_vertices("Seattle", "San Francisco")
+    city_graph.add_edge_by_vertices("San Francisco", "Riverside")
+    city_graph.add_edge_by_vertices("San Francisco", "Los Angeles")
+    city_graph.add_edge_by_vertices("Los Angeles", "Riverside")
+    city_graph.add_edge_by_vertices("Los Angeles", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Chicago")
+    city_graph.add_edge_by_vertices("Phoenix", "Dallas")
+    city_graph.add_edge_by_vertices("Phoenix", "Houston")
+    city_graph.add_edge_by_vertices("Dallas", "Chicago")
+    city_graph.add_edge_by_vertices("Dallas", "Atlanta")
+    city_graph.add_edge_by_vertices("Dallas", "Houston")
+    city_graph.add_edge_by_vertices("Houston", "Atlanta")
+    city_graph.add_edge_by_vertices("Houston", "Miami")
+    city_graph.add_edge_by_vertices("Atlanta", "Chicago")
+    city_graph.add_edge_by_vertices("Atlanta", "Washington")
+    city_graph.add_edge_by_vertices("Atlanta", "Miami")
+    city_graph.add_edge_by_vertices("Miami", "Washington")
+    city_graph.add_edge_by_vertices("Chicago", "Detroit")
+    city_graph.add_edge_by_vertices("Detroit", "Boston")
+    city_graph.add_edge_by_vertices("Detroit", "Washington")
+    city_graph.add_edge_by_vertices("Detroit", "New York")
+    city_graph.add_edge_by_vertices("Boston", "New York")
+    city_graph.add_edge_by_vertices("New York", "Philadelphia")
+    city_graph.add_edge_by_vertices("Philadelphia", "Washington")
+    print(city_graph)
