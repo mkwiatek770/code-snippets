c3532c7658291b96df7ffd7c76b9dad8ff5aa4ab
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-03 12:33:56 +0200

    Return to compressing scheme that was presented by author

--- Classic-CS-Problems/1-Small-Problems/exercises/exercise_2.py
+++ Classic-CS-Problems/1-Small-Problems/exercises/exercise_2.py
@@ -9,7 +9,6 @@ from sys import getsizeof
 class CompressedGene:
     def __init__(self, gene: str) -> None:
         self.bit_string = self._compress(gene)
-        print(getsizeof(self.bit_string))
 
     def __str__(self):
         return self.decompress()
@@ -25,35 +24,53 @@ class CompressedGene:
             key = (key * 4)
             return self.decompress()[key:key+4]
 
-    # def _compress(self, gene: str) -> None:
-    #     self.bit_string: int = 1  # start with sentinel
-    #     for nucleotide in gene.upper():
-    #         self.bit_string <<= 2  # shift left two bits
-    #         if nucleotide == "A":  # change last two bits to 00
-    #             self.bit_string |= 0b00
-    #         elif nucleotide == "C":  # change last two bits to 01
-    #             self.bit_string |= 0b01
-    #         elif nucleotide == "G":  # change last two bits to 10
-    #             self.bit_string |= 0b10
-    #         elif nucleotide == "T":  # change last two bits to 11
-    #             self.bit_string |= 0b11
-    #         else:
-    #             raise ValueError("Invalid Nucleotide:{}".format(nucleotide))
-    
-    def bytes_to_int(self, bytes_s: bytes) -> int:
-        result = 0
-        for b in bytes_s:
-            result = result * 256 + int(b)
-        return result
-
     def _compress(self, gene: str) -> None:
-        gene_bytes: bytes = gene.encode("utf-8")
-        return self.bytes_to_int(gene_bytes)
-        # self.bit_string = int.from_bytes(gene_bytes, "big")
-
+        bit_string: int = 1  # start with sentinel
+        for nucleotide in gene.upper():
+            bit_string <<= 2  # shift left two bits
+            if nucleotide == "A":  # change last two bits to 00
+                bit_string |= 0b00
+            elif nucleotide == "C":  # change last two bits to 01
+                bit_string |= 0b01
+            elif nucleotide == "G":  # change last two bits to 10
+                bit_string |= 0b10
+            elif nucleotide == "T":  # change last two bits to 11
+                bit_string |= 0b11
+            else:
+                raise ValueError("Invalid Nucleotide:{}".format(nucleotide))
+        return bit_string
+    
     def decompress(self) -> str:
-        temp: bytes = self.bit_string.to_bytes((self.bit_string.bit_length()+ 7) // 8, "little")
-        return temp.decode("utf-8")[::-1]
+        gene: str = ""
+        for i in range(0, self.bit_string.bit_length() - 1, 2):  # - 1 to exclude
+        # sentinel
+            bits: int = self.bit_string >> i & 0b11  # get just 2 relevant bits
+            if bits == 0b00:  # A
+                gene += "A"
+            elif bits == 0b01:  # C
+                gene += "C"
+            elif bits == 0b10:  # G
+                gene += "G"
+            elif bits == 0b11:  # T
+                gene += "T"
+            else:
+                raise ValueError("Invalid bits:{}".format(bits))
+        return gene[::-1]  # reverses string by slicing backward
+
+    # def bytes_to_int(self, bytes_s: bytes) -> int:
+    #     result = 0
+    #     for b in bytes_s:
+    #         result = result * 256 + int(b)
+    #     return result
+
+    # def _compress(self, gene: str) -> None:
+    #     gene_bytes: bytes = gene.encode("utf-8")
+    #     return self.bytes_to_int(gene_bytes)
+    #     # self.bit_string = int.from_bytes(gene_bytes, "big")
+
+    # def decompress(self) -> str:
+    #     temp: bytes = self.bit_string.to_bytes((self.bit_string.bit_length()+ 7) // 8, "little")
+    #     return temp.decode("utf-8")[::-1]
 
     # def _compress(self, gene: str) -> None:
     #     gene_bytes: bytes = gene.encode()
@@ -63,22 +80,7 @@ class CompressedGene:
     #     gene_bytes: bytes = self.bit_string.to_bytes(self.bit_string.bit_length(), "little")
     #     return gene_bytes.decode()
 
-    # def decompress(self) -> str:
-    #     gene: str = ""
-    #     for i in range(0, self.bit_string.bit_length() - 1, 2):  # - 1 to exclude
-    #     # sentinel
-    #         bits: int = self.bit_string >> i & 0b11  # get just 2 relevant bits
-    #         if bits == 0b00:  # A
-    #             gene += "A"
-    #         elif bits == 0b01:  # C
-    #             gene += "C"
-    #         elif bits == 0b10:  # G
-    #             gene += "G"
-    #         elif bits == 0b11:  # T
-    #             gene += "T"
-    #         else:
-    #             raise ValueError("Invalid bits:{}".format(bits))
-    #     return gene[::-1]  # [::-1] reverses string by slicing backward
+
 
 
 if __name__ == "__main__":
@@ -91,5 +93,8 @@ if __name__ == "__main__":
     print("compressed is {} bytes".format(getsizeof(compressed.bit_string)))
     print("original and decompressed are the same: {}".format(original == compressed.decompress()))
 
-    sequemce: str = "TAGGGATT"
-    compressed: CompressedGene = CompressedGene(sequemce)
+    sequence: str = "TAGGGATTATGT"
+    compressed: CompressedGene = CompressedGene(sequence)
+    print(sequence[0])
+    print(sequence[-1])
+    print(sequence[0:2])
