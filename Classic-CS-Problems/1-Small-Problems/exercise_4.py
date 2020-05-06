05808c84fd79147eb369a45e84242ec60ab6de1d
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-06 20:01:11 +0200

    Encrypt image

--- Classic-CS-Problems/1-Small-Problems/exercise_4.py
+++ Classic-CS-Problems/1-Small-Problems/exercise_4.py
@@ -1,3 +1,33 @@
 """
 Use a one-time pad to encrypt and decrypt images.
 """
+from typing import Tuple
+from random import randint
+from encryption import random_key
+
+
+def encrypt_image(image_path: str):
+    with open(image_path, "rb") as image:
+        image = bytearray(image.read())
+        key = randint(1, 20)
+        for index, value in enumerate(image):
+            image[index] = value^key
+        return image, key
+
+
+def save_image(image_bytes: bytearray, file_name="encrypted.jpg"):
+    import pdb
+    pdb.set_trace()
+    with open(file_name, "wb") as image:
+        image.write(bytes(image_bytes))
+
+def decrypt_image(key1: int, key2: int) -> bytes:
+    pass
+
+
+if __name__ == "__main__":
+    image_path = './image.jpg'
+    encrypted, key2 = encrypt_image(image_path)
+    save_image(encrypted)
+    
+
