fc48f80afcd381dbabe6aa17eabdbb2d0efabf52
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-09 20:45:08 +0200

    DFS go through each element

--- Algorithms/Searching/dfs.py
+++ Algorithms/Searching/dfs.py
@@ -13,12 +13,11 @@ Space: O(|V|)
 V - vertices
 E - edges
 """
-from typing import TypeVar, List
+from typing import TypeVar, List, Generic, Set
 
 T = TypeVar('T')
 
-class Stack:
-
+class Stack(Generic[T]):
     def __init__(self):
         self._container: List[T] = []
     
@@ -33,6 +32,28 @@ class Stack:
     def push(self, item: T) -> None:
         self._container.append(item)
 
+class Node:
+    def __init__(self, value: T):
+        self.value = value
+        self.adjacent: List[Node] = []
+
+
+def dfs(start_node: Node) -> None:
+    """Dfs which print all visited nodes."""
+    stack = Stack()
+    explored: Set[Node] = set()
+    stack.push(start_node)
+
+    while not stack.empty:
+        current = stack.pop()
+        if current not in explored:
+            print(f"{current.value}")
+            explored.add(current)
+
+        for neighbour in current.adjacent:
+            if neighbour not in explored:
+                stack.push(neighbour)
+
 
 if __name__ == "__main__":
     stack = Stack()
@@ -40,3 +61,23 @@ if __name__ == "__main__":
     stack.push(1)
     assert stack.pop() == 1
     assert stack.pop() == 123
+
+    A = Node('A')
+    B = Node('B')
+    C = Node('C')
+    D = Node('D')
+    E = Node('E')
+    G = Node('G')
+    F = Node('F')
+    H = Node('H')
+
+    C.adjacent = [B, D, E, F]
+    B.adjacent = [A, D, C]
+    A.adjacent = [G, B]
+    H.adjacent = [G, E]
+    G.adjacent = [A, H, F]
+    D.adjacent = [B, C, E]
+    E.adjacent = [C, D, H, F]
+    F.adjacent = [G, C, E]
+
+    dfs(C)
