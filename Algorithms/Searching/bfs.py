ea0b076f626c8ea0adf52c43120a4ddba9fc3a4c
mkwiatek770 <michalkwiatek9@o2.pl>
2020-05-10 17:11:27 +0200

    Implement bfs algorithm

--- Algorithms/Searching/bfs.py
+++ Algorithms/Searching/bfs.py
@@ -9,7 +9,7 @@ finding "hops" or distance out or "levels" away
 
 Goes Wide
 """
-from typing import TypeVar, Generic, Collection, Optional
+from typing import TypeVar, Generic, Collection, Optional, Set
 from dfs import Node
 
 T = TypeVar('T')
@@ -34,10 +34,39 @@ class Queue(Generic[T]):
             return self._container.pop(0)
 
 
+def bfs(start_node: Node) -> None:
+    queue = Queue()
+    explored: Set[Node] = set()
+    queue.push(start_node)
+
+    while not queue.empty:
+        current = queue.pop()
+        if current not in explored:
+            print(current.value)
+            explored.add(current)
+        for adj in current.adjacent:
+            if adj not in explored:
+                queue.push(adj)
 
 
 if __name__ == "__main__":
-    q = Queue([1, 2, 3, 4])
-    q.push(5)
-    assert q.pop() == 1
-    assert q.pop() == 2
+    
+    A = Node('A')
+    B = Node('B')
+    C = Node('C')
+    D = Node('D')
+    E = Node('E')
+    G = Node('G')
+    F = Node('F')
+    H = Node('H')
+
+    A.adjacent = [B, C, D, E]
+    B.adjacent = [A, G, C]
+    C.adjacent = [A, B, D]
+    D.adjacent = [C, H, E, A]
+    E.adjacent = [F, A, D]
+    G.adjacent = [F, B]
+    H.adjacent = [F, D]
+    F.adjacent = [H, E, G]
+
+    bfs(A)
