0d248a4530e1fc0ee91a443e466ff0c54ea0d646
Michal Kwiatek <michalakwiatek@gmail.com>
2022-02-22 19:20:29 +0100

    dining philosophers solution using acquire contextmanager

---
+++ Libraries/threading_/dining-philosophers-2.py
@@ -0,0 +1,50 @@
+"""
+Source: Python Cookbook 3rd edition
+"""
+import threading
+from contextlib import contextmanager
+
+# Thread-local state to stored information on locks already acquired
+_local = threading.local()
+
+
+@contextmanager
+def acquire(*locks):
+    # Sort locks by object identifier
+    locks = sorted(locks, key=lambda x: id(x))
+
+    # Make sure lock order of previously acquire locks is not violated
+    acquired = getattr(_local, 'acquired', [])
+    if acquired and max(id(lock) for lock in acquired) >= id(locks[0]):
+        raise RuntimeError('Lock Order Violation')
+
+    # Acquire all of the locks
+    acquired.extend(locks)
+    _local.acquired = acquired
+    try:
+        for lock in locks:
+            lock.acquire()
+        yield
+    finally:
+        # Release locks in reverse order of acquisition
+        for lock in reversed(locks):
+            lock.release()
+        del acquired[-len(locks):]
+
+
+# Philosopher thread
+def philosopher(left, right):
+    while True:
+        with acquire(left, right):
+            print(threading.currentThread(), 'eating')
+
+
+# chopsticks (represented by locks)
+NSTICKS = 5
+chopsticks = [threading.Lock() for _ in range(NSTICKS)]
+
+
+# Create all of the philosophers
+for n in range(NSTICKS):
+    t = threading.Thread(target=philosopher, args=(chopsticks[n], chopsticks[(n+1) % NSTICKS]))
+    t.start()
